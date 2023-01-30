88ca62250165190627770992fe9be961a3269709
eni86 <radha.jg@gmail.com>
2023-01-30 02:24:18 -0300

    segunda version

--- combinatorics.py
+++ combinatorics.py
@@ -9,12 +9,29 @@ def isNumber(m):
 	return type(m) == int
 
 def m_greater_than_n (m,n):
-	'''It checks that the first number '''
+	'''It checks that the first number is greater than the second'''
+	return m >= n
+
+##################################################################
+
+##################Pregunta si desea continuar ##################
+
+def keepGoing():
+	'''This function asks the user if he wants to keep operating.
+	It contemplates 3 options: yes, no or I don't get
+	wht do you want me to do'''
+	sigo = input("Desea continuar? Si/nO")
+	if sigo.lower() == 'no':
+		return False
+	elif sigo.lower() == 'si':
+		return True
+	else:
+		print("No entiendo lo que se pide")
+		keepGoing()
 
 
 
-
-################Operations######################################
+################Operations########################################
 def calc_combinations(m, n):
 	''' This function returns the combinations
 	of two numbers'''
@@ -31,3 +48,36 @@ def calc_permutations(m):
 	return factorial(m)
 
 ################################################################
+
+##########################MENU PRINCIPAL####################
+def Menu():
+	print ("Opciones del programa:")
+	print("1) Calcular arreglos")
+	print("2) Calcular permutaciones")
+	print("3) Calcular combinaciones")
+	print("4) Salir")
+
+
+
+
+###################MAIN############################
+def main ():
+	print ("Bienvenido a Combinatorics")
+	sigo = True
+	while sigo:
+		Menu()
+#################################################
+
+
+		try:
+			option = int("Que opcion elijes?")
+		except ValueError:
+			print ('No entiendo que desear hacer')
+		else:
+			pass
+
+
+
+		pass
+if __name__ == '__main__':
+	main()
